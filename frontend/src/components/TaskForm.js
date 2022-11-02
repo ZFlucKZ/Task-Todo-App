bcb5dee547e30fd2fad6edc863f53e69ae110457
Fluke <guyguy5050@gmail.com>
2022-11-02 15:52:14 +0700

    Components and styles

--- frontend/src/components/TaskForm.js
+++ frontend/src/components/TaskForm.js
@@ -1,5 +1,16 @@
-const TaskForm = () => {
-  return <div>TaskForm</div>;
+const TaskForm = ({ createTask, name, handleInputChange }) => {
+  return (
+    <form className="task-form" onSubmit={createTask}>
+      <input
+        type="text"
+        placeholder="Add a Task"
+        name="name"
+        value={name}
+        onChange={handleInputChange}
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
 };
 
 export default TaskForm;
--- frontend/src/components/TaskList.js
+++ frontend/src/components/TaskList.js
@@ -1,5 +1,23 @@
+import Task from './Task';
+import TaskForm from './TaskForm';
+
 const TaskList = () => {
-  return <div>TaskList</div>;
+  return (
+    <div className="task-list">
+      <h1 className="title">Task to do List</h1>
+      <TaskForm />
+      <div className="flex-between">
+        <p>
+          <b>Total Tasks:</b> 0
+        </p>
+        <p>
+          <b>Completed Tasks:</b> 0
+        </p>
+      </div>
+      <hr />
+      <Task />
+    </div>
+  );
 };
 
 export default TaskList;
