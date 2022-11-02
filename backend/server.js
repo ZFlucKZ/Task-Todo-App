f797d0b21778bf4df1d8bfb8a4be69ded4749dd0
Fluke <guyguy5050@gmail.com>
2022-11-02 10:41:36 +0700

    Connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,23 @@
+const dotenv = require('dotenv').config();
 const express = require('express');
+const connectDB = require('./config/connectDB');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Routes
+app.get('/', (req, res) => {
+  res.send('Home Page');
 });
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
