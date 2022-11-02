f3318161146498cca55b671e2de802e049f619f2
Fluke <guyguy5050@gmail.com>
2022-11-02 11:29:38 +0700

    Saving task to database

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,28 @@ const dotenv = require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/connectDB');
 const mongoose = require('mongoose');
+const Task = require('./model/taskSchema');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // Routes
 app.get('/', (req, res) => {
   res.send('Home Page');
+});
+
+// Create a Task
+app.post('/api/tasks', async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 mongoose
