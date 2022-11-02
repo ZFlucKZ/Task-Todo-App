96940d2fd6c98a3af93d84970f1d08730db775b6
Fluke <guyguy5050@gmail.com>
2022-11-02 16:18:34 +0700

    Add task to database

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const connectDB = require('./config/connectDB');
 const mongoose = require('mongoose');
 const Task = require('./models/taskSchema');
 const taskRoute = require('./routes/taskRoute');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,6 +12,8 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
 app.use('/api/tasks', taskRoute);
 
 // Routes
